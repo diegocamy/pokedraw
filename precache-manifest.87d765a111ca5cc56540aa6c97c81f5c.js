01e905d92a4a66d7e6b0b045abb9830d3a610e00
diegocamy <diegocamyy@gmail.com>
2020-06-06 23:05:57 -0300

    Updates

---
+++ precache-manifest.87d765a111ca5cc56540aa6c97c81f5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e1f26ea9fa374c79c1b7911b923d75",
+    "url": "/pokedraw/index.html"
+  },
+  {
+    "revision": "46c55d14bb75e8c4225b",
+    "url": "/pokedraw/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7771ec2afd9419eb84d4",
+    "url": "/pokedraw/static/css/main.6cac42dc.chunk.css"
+  },
+  {
+    "revision": "46c55d14bb75e8c4225b",
+    "url": "/pokedraw/static/js/2.f75a2e49.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pokedraw/static/js/2.f75a2e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7771ec2afd9419eb84d4",
+    "url": "/pokedraw/static/js/main.8f5e1cbb.chunk.js"
+  },
+  {
+    "revision": "91d99974e397c52a36e4",
+    "url": "/pokedraw/static/js/runtime-main.ebd702fc.js"
+  },
+  {
+    "revision": "ae8bcdf7889513429196bf25aadb0b8f",
+    "url": "/pokedraw/static/media/logo.ae8bcdf7.png"
+  }
+]);
